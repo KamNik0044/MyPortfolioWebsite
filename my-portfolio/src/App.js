acaccde3d7210414ca87f1dce17b6e9bf0e78778
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-07-14 18:56:20 +0100

    UpdateApp.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -5,12 +5,19 @@ import Contact from "./routes/Contact";
 import About from "./routes/About";
 import Project from "./routes/Project";
 
+import { Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div>
-  
-    </div>
+    <>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+     </Routes>
+    </>
   );
 }
 
