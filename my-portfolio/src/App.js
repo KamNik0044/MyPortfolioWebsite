0b56b03194a3aee3d33595f9c98015f54253ef08
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-07-14 18:46:57 +0100

    Navbar Links Imported

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 import "./index.css";
+import Home from "./routes/Home";
+import Contact from "./routes/Contact";
+import About from "./routes/About";
+import Project from "./routes/Project";
+
+
 function App() {
   return (
     <div>
