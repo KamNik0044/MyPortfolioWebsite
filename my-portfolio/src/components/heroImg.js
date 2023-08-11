778df25999f8a457b150c8a2e14c3e713a48615b
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-08-11 15:57:34 +0100

    projectContactBtns

--- my-portfolio/src/components/heroImg.js
+++ my-portfolio/src/components/heroImg.js
@@ -3,12 +3,21 @@ import "./HeroImgStyles.css";
 import React from "react";
 
 import IntroImg from "../assets/intro-bg.jpg"
+import { Link } from "react-router-dom";
 
 const heroImg = () => {
   return (
     <div className="hero">
       <div className="mask">
         <img className="into-img" src={IntroImg} alt="IntroImg"/>
+      </div>
+      <div className="content">
+        <p>Hi, I AM A DEVELOPER</p>
+        <h1>React Developer</h1>
+        <div>
+            <Link to="/project" className="btn">Projects</Link>
+            <Link to="/contact" className="btn-light">Contact</Link>
+        </div>
       </div>
     </div>
   )
