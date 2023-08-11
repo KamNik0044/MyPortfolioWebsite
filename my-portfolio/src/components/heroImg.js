aee2f4f2944c72d9a42af17aa993a4b54e1a5fe2
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-08-11 15:26:39 +0100

    exportHeroImg

--- my-portfolio/src/components/heroImg.js
+++ my-portfolio/src/components/heroImg.js
@@ -1,1 +1,17 @@
 import "./HeroImgStyles.css";
+
+import React from 'react'
+
+const heroImg = () => {
+  return (
+    <div className="hero">
+      <div className="mask">
+
+      </div>
+    </div>
+  )
+}
+
+export default heroImg;
+
+
