59213aa612ed1d0906e1afe11afbb9d96053c415
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-08-22 19:35:28 +0100

    phoneMailIcons

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -11,6 +11,22 @@ const Footer = () => {
             <div className="left">
                 <div className="location">
                     <FaHome size={20} style={{ color:"fff", marginRight: "2rem" }} />
+                    <div>
+                        <p>3 Ashford Court, Stockport</p>
+                        <p>Manchester</p>
+                    </div>
+                </div>
+                <div className="phone">
+                    <h4>
+                    <FaPhone size={20} style={{ color:"fff", marginRight: "2rem" }} />
+                    1-2323-343-23
+                    </h4>
+                </div>
+                <div className="emaol">
+                    <h4>
+                    <FaMail size={20} style={{ color:"fff", marginRight: "2rem" }} />
+                    1-2323-343-23
+                    </h4>
                 </div>
             </div>
             <div className="right"></div>
