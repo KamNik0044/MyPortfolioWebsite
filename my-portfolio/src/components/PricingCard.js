a56c5693c9a7d6c66c89b4f1fb39531e4783de46
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-10-08 19:05:33 +0100

    pricingPargraphsAdded

--- my-portfolio/src/components/PricingCard.js
+++ my-portfolio/src/components/PricingCard.js
@@ -4,8 +4,18 @@ import React from 'react'
 
 const PricingCard = () => {
   return (
-    <div>
-      
+    <div className="pricing">
+      <div className="card-container">
+        <div className="card">
+            <h3>- Basic -</h3>
+            <span className="bar"></span>
+            <p className="btc">$ 100</p>
+            <p>- 3 Days -</p>
+            <p>- 3 Pages -</p>
+            <p>- Featured -</p>
+            <p>- Responsive Design -</p>
+        </div>
+      </div>
     </div>
   )
 }
