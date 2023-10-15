66fdf94edfedc7c734265ee893f58b0bb28d7bc5
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-10-15 18:29:54 +0100

    workCardUpdate1

--- my-portfolio/src/components/WorkCard.js
+++ my-portfolio/src/components/WorkCard.js
@@ -1,11 +1,17 @@
-import "./WorkCardStyles.css"
+import "./WorkCardStyles.css";
 
-import React from 'react'
+import React from 'react';
+import pro1 from "../assets/project1.png"
 
 const WorkCard = () => {
   return (
-    <div>
-      
+    <div className="work-container">
+      <h1 className="project-heading">Projects</h1>
+      <div className="project-container">
+        <div className="project-card">
+            <img src="" alt="image"/>
+        </div>
+      </div>
     </div>
   )
 }
--- my-portfolio/src/routes/Project.js
+++ my-portfolio/src/routes/Project.js
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroImg2 from '../components/HeroImg2'
 import PricingCard from '../components/PricingCard'
+import WorkCard from '../components/WorkCard'
 
 const Project = () => {
   return (
@@ -11,6 +12,7 @@ const Project = () => {
       <Navbar/>
       <Footer/>
       <HeroImg2 heading="PROJECTS." text="Some of my most recent works" />
+      <WorkCard />
       <PricingCard />
     </div>
   )
