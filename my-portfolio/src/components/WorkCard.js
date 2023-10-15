c1414cd830399886e8ea5c75aa9118034dd0cf98
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-10-15 18:50:44 +0100

    workCardUpdate2

--- my-portfolio/src/components/WorkCard.js
+++ my-portfolio/src/components/WorkCard.js
@@ -1,7 +1,8 @@
 import "./WorkCardStyles.css";
 
 import React from 'react';
-import pro1 from "../assets/project1.png"
+import pro1 from "../assets/project1.png";
+import { NavLink } from "react-router-dom";
 
 const WorkCard = () => {
   return (
@@ -9,7 +10,14 @@ const WorkCard = () => {
       <h1 className="project-heading">Projects</h1>
       <div className="project-container">
         <div className="project-card">
-            <img src="" alt="image"/>
+            <img src={pro1} alt="image"/>
+            <h2 className="project-title">Project Title</h2>
+            <div className="pro-details">
+                <p>This is Text</p>
+                <div className="pro-btns">
+                    <NavLink></NavLink>
+                </div>
+            </div>
         </div>
       </div>
     </div>
